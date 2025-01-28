55f6e0e269af947f9a41383aeca0788b97028aac
gdurand2 <gdurand2@illinois.edu>
2025-01-27 23:21:44 -0600

    added starter code

---
+++ src/solution.cc
@@ -0,0 +1,12 @@
+#include "solution.hpp"
+
+std::vector<int> RotateRight(const std::vector<int>& vect,
+                             unsigned int no_rotations) {
+  // TODO
+}
+
+std::vector<int> RotateChunks(const std::vector<int>& vect,
+                              unsigned int chunk_size,
+                              unsigned int no_rotations) {
+  // TODO
+}
