f2041ea390f01fc6c668de69cd69b448e9301de9
michael <mnowak1@illinois.edu>
2025-09-05 10:17:16 -0500

    MakeFile

--- src/solution.cc
+++ src/solution.cc
@@ -3,10 +3,12 @@
 std::vector<int> RotateRight(const std::vector<int>& vect,
                              unsigned int no_rotations) {
   // TODO
+  return vect;
 }
 
 std::vector<int> RotateChunks(const std::vector<int>& vect,
                               unsigned int chunk_size,
                               unsigned int no_rotations) {
   // TODO
+  return vect;
 }
